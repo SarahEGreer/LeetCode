b3c06083ae2e6fe1cce9ffbcd7306c4b08edb5f8
Sarah Greer <140839981+SarahEGreer@users.noreply.github.com>
2024-06-21 14:30:06 -0400

    Time: 5 ms (57.5%), Space: 44.5 MB (57.41%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+       
+       int total = 0;
+       int previousValue = 0;
+
+       for(int i = 0; i < s.length(); i++){
+        int currentValue = romanMap.get(s.charAt(i));
+
+        if(currentValue > previousValue){
+            total += currentValue - 2 * previousValue;
+        } else {
+            total += currentValue;
+        }
+        previousValue = currentValue;
+       }
+       
+       return total;
+
+    }
+
+    
+}
